d6505a142d13a228f245066bd3c20d9a5c0a1cf4
phearoak <phearoak@gmail.com>
2024-05-06 21:45:13 +0200

    add serie crud and service

---
+++ server/src/main/java/com/ggkps/superflix/api/SerieCRUD.java
@@ -0,0 +1,67 @@
+package com.ggkps.superflix.api;
+
+import com.ggkps.superflix.entities.Serie;
+import com.ggkps.superflix.models.SerieContent;
+import com.ggkps.superflix.repositories.SerieRepository;
+import com.ggkps.superflix.services.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class SerieCRUD {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    @Autowired
+    private SerieService serieService;
+
+    public SerieCRUD() {
+    }
+
+    @PostMapping("/serie/")
+    public String createSerie(@RequestBody SerieContent serieContent) {
+        Serie newSerie = serieService.createSerie(serieContent);
+
+        System.out.println(newSerie);
+
+        if (newSerie != null) {
+            return newSerie.toString();
+        }
+
+        return "Serie not created";
+    }
+
+    @GetMapping("/serie/{serie_id}")
+    public String readSerie(@PathVariable("serie_id") Long serie_id) {
+        Optional<Serie> serie = serieRepository.findById(serie_id);
+
+        if (serie.isPresent()) {
+            return serie.get().toString();
+        }
+
+        return "Serie not found";
+    }
+
+    @PatchMapping("/serie/{serie_id}")
+    public String updateSerie(@PathVariable("serie_id") long serie_id, @RequestBody SerieContent serieContent) {
+        Optional<Serie> movie = serieRepository.findById(serie_id);
+
+        if (movie.isEmpty()) {
+            return "Movie not found";
+        }
+
+        Serie updatedMovie = serieService.updateSerie(serie_id, serieContent);
+
+        return updatedMovie.toString();
+    }
+
+    @DeleteMapping("/serie/{serie_id}")
+    public String deleteSerie(@PathVariable("serie_id") Long serie_id) {
+        boolean exists = serieService.deleteSerie(serie_id);
+
+        return exists ? "Movie deleted" : "Movie not found";
+    }
+}
---
+++ server/src/main/java/com/ggkps/superflix/services/SerieService.java
@@ -0,0 +1,70 @@
+package com.ggkps.superflix.services;
+
+import com.ggkps.superflix.entities.Movie;
+import com.ggkps.superflix.entities.Serie;
+import com.ggkps.superflix.entities.VisualContent;
+import com.ggkps.superflix.models.SerieContent;
+import com.ggkps.superflix.repositories.SerieRepository;
+import com.ggkps.superflix.repositories.VisualContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    @Autowired
+    private VisualContentRepository visualContentRepository;
+
+    public SerieService() {
+    }
+
+    public Serie createSerie(SerieContent serieContent) {
+        VisualContent visualContent = new VisualContent()
+                .setTitle(serieContent.getTitle())
+                .setReleaseAt(serieContent.getReleaseAt() != null ? serieContent.getReleaseAt() : new Date())
+                .setCategory(serieContent.getCategory())
+                .setCreator(serieContent.getCreator())
+                .setDescription(serieContent.getDescription());
+
+        Serie newSerie = new Serie(serieContent.getNumberOfSeasons(), visualContent);
+        visualContentRepository.save(visualContent);
+        return serieRepository.save(newSerie);
+    }
+
+    public Serie updateSerie(long id, SerieContent serieContent) {
+        Optional<Serie> serie = serieRepository.findById(id);
+
+        if (serie.isEmpty()) {
+            return null;
+        }
+
+        VisualContent visualContent = serie.get().getVisualContent();
+        if (serieContent.getTitle() != null) visualContent.setTitle(serieContent.getTitle());
+        if (serieContent.getReleaseAt() != null) visualContent.setReleaseAt(serieContent.getReleaseAt());
+        if (serieContent.getCategory() != null) visualContent.setCategory(serieContent.getCategory());
+        if (serieContent.getCreator() != null) visualContent.setCreator(serieContent.getCreator());
+        if (serieContent.getDescription() != null) visualContent.setDescription(serieContent.getDescription());
+        if (serieContent.getNumberOfSeasons() != null) serie.get().setNumberOfSeasons(serieContent.getNumberOfSeasons());
+
+        visualContentRepository.save(visualContent);
+        return serieRepository.save(serie.get());
+    }
+
+    public boolean deleteSerie(long id) {
+        Optional<Serie> serie = serieRepository.findById(id);
+
+        if (serie.isPresent()) {
+            visualContentRepository.deleteById(serie.get().getVisualContent().getId());
+            serieRepository.deleteById(id);
+            serieRepository.flush();
+            return true;
+        }
+        return false;
+    }
+}

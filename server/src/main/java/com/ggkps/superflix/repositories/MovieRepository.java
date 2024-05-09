a9dd242164cd63a7aa60b2e827569319ad45180f
phearoak <phearoak@gmail.com>
2024-05-10 00:21:51 +0200

    add query to get movie by user

--- server/src/main/java/com/ggkps/superflix/repositories/MovieRepository.java
+++ server/src/main/java/com/ggkps/superflix/repositories/MovieRepository.java
@@ -2,5 +2,18 @@ package com.ggkps.superflix.repositories;
 
 import com.ggkps.superflix.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query(value = "SELECT movie.* " +
+            "FROM movie " +
+            "LEFT JOIN visual_content ON movie.visual_content_id = visual_content.id " +
+            "WHERE movie.id = :id", nativeQuery = true)
+    List<Movie> findMoviesByUserId(@Param("id") Long id);
+
+}
